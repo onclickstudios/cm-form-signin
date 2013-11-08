2bdaa929494c5c3690d4644691d52b8887bed91d
Tom Brennan <tjb1982@gmail.com>
2013-11-08 09:06:55 -0500

    removed cmp-form dependency

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var form = require('cmp-form');
+var
+hoquet = require('hoquet');
 
-module.exports = function() {
-  return form({
-    action: '/auth/local',
-    inputs: [{type: 'text', name: 'username', placeholder: 'username'},
-             {type: 'password', name: 'password', placeholder: 'password'},
-             {type: 'submit', name: 'submit', value: 'go'}]
-  });
+/**
+ * 
+ * @param action String form action
+ * @param data Object optional data to prepopulate the form
+ */
+module.exports = function cmpFormSingin(action, data) {
+  
+  return hoquet.render(
+    ['form',
+     {action:action, method:'post'},
+     ['input', {type:'text', name:'username', placeholder:'username', value:data && data.username}],
+     ['input', {type:'password', name:'password', placeholder:'password', value:data && data.password}],
+     ['input', {type:'submit', name:'submit', value:data && data.submit || 'submit'}]]
+  );
+  
 };
