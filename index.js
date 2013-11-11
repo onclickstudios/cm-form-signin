d7cc0623e9c35f211c68e79c582f7a6968e054ac
Tom Brennan <tjb1982@gmail.com>
2013-11-11 16:11:36 -0500

    bulk

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var exports = module.exports = function cmFormSignin(context) {
   return hoquet.render(
     ['form',
      {action:context.action + 
-             (context.redirect_uri ? '?' + context.redirect_uri : ''),
+             (context.redirect_uri ? '?redirect_uri=' + context.redirect_uri : ''),
       method:(context.method || 'post'),
       class:'cm cm-form cm-form-signin' + (context && context.class || '')},
      ['input', {type:'text', name:'username', placeholder:'username', value:data.username}],
